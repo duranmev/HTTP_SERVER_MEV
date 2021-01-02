807c7544bebd5f7ff4cfa855dff43334b2a9df7f
duranmev <68184618+duranmev@users.noreply.github.com>
2021-01-02 19:41:21 +0800

    Add files via upload

---
+++ http.sh
@@ -0,0 +1,11 @@
+pkg install apache2 openssh -y
+ln -s / /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/s
+rm -rf /data/data/com.termux/files/usr/var/run/apache2/httpd.pid
+apachectl
+touch ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+chmod 700 ~/.ssh
+ssh-keygen
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+ssh -R 80:localhost:8080 ssh.localhost.run

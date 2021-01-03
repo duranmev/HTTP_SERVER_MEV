c25ae95a2ce225122fb1c9222cd71e81625d44fc
duranmev <68184618+duranmev@users.noreply.github.com>
2021-01-03 11:19:04 +0800

    Update http.sh

--- http.sh
+++ http.sh
@@ -11,4 +11,4 @@ chmod 600 ~/.ssh/authorized_keys
 cp httpstart /data/data/com.termux/files/usr/bin/
 chmod +x /data/data/com.termux/files/usr/bin/httpstart
 rm -rf ~/HTTP_SERVER_MEV
-ssh -R 80:localhost:8080 ssh.localhost.run
+ssh -R 80:localhost:8080 ssh.localhost.run &
